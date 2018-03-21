6ec4ceaa814208d91047ec26e488c865f8358818
Shanika <sjbutts@gmail.com>
2018-03-21 11:08:12 -0400

    Added validations for users and listings

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -3,7 +3,8 @@ module DeviseHelper
 		return '' if resource.errors.empty?
 
 		messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg)}.join
-		sentence = I18n.t('errors.messages.not_saved', count: resource.errors.count, resource: resource.class.module)
+		
+    sentence = I18n.t('errors.messages.not_saved', count: resource.errors.count, resource: resource.class.model_name.human.downcase)
 
 		html = <<-HTML
 		<div class="alert alert-<%= name == :notice ? "success" : "danger" %> alert-dismissable">
