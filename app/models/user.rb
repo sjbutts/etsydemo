9d7f4eda95da54908959cd84fa60a1f05c6420df
Shanika <sjbutts@gmail.com>
2018-04-05 17:06:30 -0400

    Enable users to place orders

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
    validates :name, presence: true
 
    has_many :listings, dependent: :destroy #The listing is dependent upon the user that created it, if the user is deleted the listing is deleted as well
-
+   has_many :sales, class_name: "Order", foreign_key: "seller_id"
+   has_many :purchases, class_name: "Order", foreign_key: "buyer_id"
 
 end
